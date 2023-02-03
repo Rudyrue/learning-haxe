74c88816b726ea6ddfa63e5bf6d196a63cae29a0
Rudyrue <73214127+Rudyrue@users.noreply.github.com>
2023-02-03 00:31:05 -0800

    shit for tomorrow

---
+++ source/MusicTheory.hx
@@ -0,0 +1,24 @@
+package;
+
+class MusicTheory {
+	public static var bpm:Float = 120;
+	public static var measure:Int = (bpm / 4);
+	public static var crochet:Float = ((60 / bpm) * 1000);
+	public static var stepCrochet:Float = crochet / 4;
+
+	public static var curStep:Int = 1;
+	public static var curBeat:Int = 1;
+	public static var curMeasure:Int = 1;
+
+	public function measureHit() {
+
+	}
+
+	public function beatHit() {
+		
+	}
+
+	public function stepHit() {
+
+	}
+}

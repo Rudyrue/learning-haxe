33a64aad126db95f1efced46a72126caf231bd68
Rudyrue <73214127+Rudyrue@users.noreply.github.com>
2023-02-03 13:45:15 -0800

    what

--- source/MusicTheory.hx
+++ source/MusicTheory.hx
@@ -4,7 +4,7 @@ class MusicTheory extends flixel.FlxState {
 	public static var bpm:Float = 120;
 	public static var measure:Int = (bpm / 4);
 	public static var crochet:Float = ((60 / bpm) * 1000);
-	public static var stepCrochet:Float = crochet / 4;
+	public static var stepCrochet:Float = (crochet / 4);
 
 	public static var curStep:Int = 1;
 	public static var curBeat:Int = 1;

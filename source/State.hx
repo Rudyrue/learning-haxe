a67aacf1f9f55cdb320268f32c0263b1a812b53a
Rudyrue <73214127+Rudyrue@users.noreply.github.com>
2023-02-02 22:49:54 -0800

    add some text

--- source/State.hx
+++ source/State.hx
@@ -3,12 +3,17 @@ package;
 import flixel.FlxSprite;
 import flixel.FlxCamera;
 import flixel.FlxG;
+import flixel.text.FlxText;
 
 class State extends flixel.FlxState {
 	var cam:FlxCamera;
+	var txt:FlxText;
 
 	override public function create() {
 		super.create();
+
+		text = new FlxText(50, 0, 0, 'ok fjgodhfdghji', 75)
+		add(text);
 
 		cam = new FlxCamera();
 		FlxG.cameras.reset(cam);
